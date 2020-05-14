5aa128ed24358b12d8d9f5985997e41035a20171
pligopsychakis <panligo@gmail.com>
2020-05-15 00:08:07 +0300

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -26,10 +26,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch((err) => {
-    res.status(400).json("unable to register");
-   console.log(err); 
-  });
+  }).catch((err) => res.status(400).json("unable to register"));
 }
 
 module.exports = {
