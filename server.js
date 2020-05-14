ff1149e218bb6f5523e37f7d8f943d95bd1d4606
pligopsychakis <panligo@gmail.com>
2020-05-14 12:21:59 +0300

    adding database connection

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-fitted-72613",
-    user: "panligo",
-    password: "33775",
-    database: "face-recognition",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
